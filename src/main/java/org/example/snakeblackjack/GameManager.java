f0abd219a989c24aef1455d17c8099c6930d8f06
ProcrastinateGuy <ching91202@gmail.com>
2025-05-10 15:07:00 -0700

    fixed calling BlackJack from Game Manager

--- src/main/java/org/example/snakeblackjack/GameManager.java
+++ src/main/java/org/example/snakeblackjack/GameManager.java
@@ -1,6 +1,9 @@
 package org.example.snakeblackjack;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GameManager extends Application {
@@ -11,7 +14,18 @@ public class GameManager extends Application {
         Stage blackJackStage = new Stage();
         Stage snakeStage = new Stage();
         SnakeGame.launchGame(snakeStage);
-        BlackJack.launchGame(blackJackStage);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/blackjack.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        }catch (Exception e){System.out.println(e.getMessage());}
+
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+        newStage.setTitle("Black Jack He He");
+        newStage.show();
+
 
     }
 
